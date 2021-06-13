0c2681c9d92dcb74c51456bfe289de0fd69006e3
Roberto Basulto <roberto.basulto085@gmail.com>
2021-06-13 16:01:42 -0400

    Added anime_search method in the anime_facade

--- app/facades/anime_facade.rb
+++ app/facades/anime_facade.rb
@@ -9,11 +9,32 @@ class AnimeFacade
             else
                 anime_list = anime_list.order(name_engl: :asc)
             end
-            
+
             anime_list = anime_list.where(season_year: params[:year_filter]) if params[:year_filter].present?
             anime_list = anime_list.where('season ILIKE ?', "%#{params[:season_filter]}%") if params[:season_filter].present?
 
             return anime_list
+        end
+
+        def anime_search(anime_title)
+            #checks if anime exists in db first
+            if anime = Anime.find_by('name_engl ILIKE ?', "%#{anime_title}%")
+                return anime
+            else
+                anime = AnilistService.anime_search(anime_title)
+                return anime if anime.has_key?(:error)
+
+                Anime.create(
+                    name_engl: anime[:title][:english],
+                    name_native: anime[:title][:native],
+                    description: anime[:description],
+                    episodes: anime[:episodes],
+                    season: anime[:season],
+                    season_year: anime[:seasonYear],
+                    score: anime[:averageScore],
+                    status: anime[:status]
+                )
+            end
         end
 
         private
